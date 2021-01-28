f58d7f8007d415f6fc00e228ca276e7f86408b50
taoyuchuan <taoyuchuan@gmail.com>
2021-01-27 19:52:44 -0500

    Add array in useState

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
 import HookCounterTwo from './components/HookCounterTwo'
 import HookCounterThree from './components/HookCounterThree'
+import HookCounterFour from './components/HookCounterFour'
 
 function App() {
   return (
     <div className="App">
+      <HookCounterFour />
       <HookCounterThree />
       <HookCounterTwo />
       <ClassCounter />
