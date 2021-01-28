0174a8456c80e599c6edd7fe9bf5b07a5ff42066
taoyuchuan <taoyuchuan@gmail.com>
2021-01-27 19:33:46 -0500

    Add object in useState

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
 import HookCounterTwo from './components/HookCounterTwo'
+import HookCounterThree from './components/HookCounterThree'
 
 function App() {
   return (
     <div className="App">
+      <HookCounterThree />
       <HookCounterTwo />
       <ClassCounter />
       <HookCounter />
