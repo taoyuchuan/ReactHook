95bdbb4bdaff66d3a6796bfdca00b8eb340638c7
taoyuchuan <taoyuchuan@gmail.com>
2021-01-29 19:26:07 -0500

    Add conditionally useEffect

--- src/components/UseEffectCounterOne.js
+++ src/components/UseEffectCounterOne.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react'
 
 function UseEffectCounterOne() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
 
   // executed after every render.
   useEffect(() => {
+    console.log('useEffect = Updating document title')
     document.title = `You clicked ${count} times`
-  })
+  }, [count])
 
   return(
     <div>
+      <input type = 'text' value = { name } onChange = { e => setName(e.target.value) }/>
       <button onClick = { () => setCount(prevCount => prevCount + 1) }>Click { count } times</button>
     </div>
   )
